f235eae9e16901f6a2c84194349fdd921c970701
Filip S <filip.syn@protonmail.com>
2023-12-04 18:30:15 +0100

    Render Swagger on root of API

--- FormUp.Api/Program.cs
+++ FormUp.Api/Program.cs
@@ -7,7 +7,12 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
