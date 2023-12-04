f697f89e6ce5d428c80cdf54d120db890c89ad3b
Filip S <filip.syn@protonmail.com>
2023-12-04 18:30:54 +0100

    Extend documentation in Swagger

--- FormUp.Api/Program.cs
+++ FormUp.Api/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "FormUp", Version = "v1",
+            Description =
+                "Server-side application to store information about workouts, gyms and fit lifestyle in general.",
+            Contact = new OpenApiContact
+            {
+                Url = new Uri("https://github.com/filipsyn/formup-server")
+            }
+        });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
