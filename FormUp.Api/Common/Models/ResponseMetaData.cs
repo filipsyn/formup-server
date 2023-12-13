b6e47cc3dfeb58536895ef5e9fb04f8f2daa5b0f
Filip S <filip.syn@protonmail.com>
2023-12-13 13:21:36 +0100

    Add documentation to `ResponseMetaData`

--- FormUp.Api/Common/Models/ResponseMetaData.cs
+++ FormUp.Api/Common/Models/ResponseMetaData.cs
@@ -1,3 +1,13 @@
 namespace FormUp.Api.Common.Models;
 
+/// <summary>
+///     Groups metadata about response payload.
+/// </summary>
+/// <param name="RetrievedCount">Number of currently retrieved items in array.</param>
+/// <param name="PageSize">Maximum number of retrieved items in payload.</param>
+/// <param name="TotalCount">Count of all available items to be retrieved.</param>
+/// <remarks>
+///     This metadata describes payloads that are arrays. Providing this information simplifies support for pagination
+///     on client side.
+/// </remarks>
 public record ResponseMetaData(int RetrievedCount, int PageSize, int TotalCount);
