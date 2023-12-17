49bb20725991d16156518b605b09315d9a051aee
Filip S <filip.syn@protonmail.com>
2023-12-17 22:31:18 +0100

    Add database seeding of exercises

--- FormUp.Api/Data/DataContext.cs
+++ FormUp.Api/Data/DataContext.cs
@@ -1,4 +1,5 @@
 using FormUp.Api.Common.Persistence;
+using FormUp.Api.Data.Configuration;
 using FormUp.Api.Features.v1.Exercises;
 using FormUp.Api.Features.v1.Gyms;
 
@@ -13,4 +14,9 @@ public class DataContext : DbContext
     public DbSet<GymEntity> Gyms => Set<GymEntity>();
     public DbSet<GeolocationEntity> Geolocations => Set<GeolocationEntity>();
     public DbSet<ExerciseEntity> Exercises => Set<ExerciseEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ExercisesEntityConfiguration());
+    }
 }
