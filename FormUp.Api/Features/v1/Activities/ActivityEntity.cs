dfad0b973ce6fe82b3c6f79839cb688be4bc6c73
Filip S <filip.syn@protonmail.com>
2023-12-19 20:39:34 +0100

    Add `Activity` entity

---
+++ FormUp.Api/Features/v1/Activities/ActivityEntity.cs
@@ -0,0 +1,36 @@
+using FormUp.Api.Common.Persistence;
+using FormUp.Api.Features.v1.Exercises;
+
+namespace FormUp.Api.Features.v1.Activities;
+
+/// <summary>
+///     Entity representing an activity logged to user's workout.
+/// </summary>
+public class ActivityEntity : BaseEntity
+{
+    /// <summary>
+    ///     Exercise performed.
+    /// </summary>
+    public ExerciseEntity? Exercise { get; set; }
+
+    /// <summary>
+    ///     Number of sets performed of one exercise in a Workout.
+    /// </summary>
+    public int SetsCount { get; set; }
+
+
+    /// <summary>
+    ///     Number of repetitions of exercise performed in each set.
+    /// </summary>
+    public int RepetitionsCount { get; set; }
+
+    /// <summary>
+    ///     Weight resistance with which the exercise is performed.
+    /// </summary>
+    public float Weight { get; set; }
+
+    /// <summary>
+    ///     Notes relating to an activity.
+    /// </summary>
+    public string Notes { get; set; } = string.Empty;
+}
