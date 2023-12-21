9543689a2a2ad08ed57dd655afcb63f0119dfb42
Filip S <filip.syn@protonmail.com>
2023-12-21 15:42:04 +0100

    Add Get endpoint on workouts controller

---
+++ FormUp.Api/Features/v1/Workouts/WorkoutsController.cs
@@ -0,0 +1,39 @@
+using FormUp.Api.Common.Config;
+using FormUp.Api.Common.Extensions;
+using FormUp.Api.Common.Models;
+using FormUp.Api.Features.v1.Shared;
+using FormUp.Contracts.v1.Workouts;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormUp.Api.Features.v1.Workouts;
+
+[ApiController]
+[Route(EndpointUrls.Workouts.GroupUrl)]
+[Produces("application/json")]
+public class WorkoutsController : ControllerBase
+{
+    private readonly IWorkoutsService _workoutsService;
+
+    public WorkoutsController(IWorkoutsService workoutsService)
+    {
+        _workoutsService = workoutsService;
+    }
+
+    [HttpGet(EndpointUrls.Workouts.Get)]
+    [ProducesResponseType<ApiResponse<IList<WorkoutInfo>>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status404NotFound)]
+    public async Task<IResult> Get(
+        [FromRoute] string userId,
+        [FromQuery] int skip = Constants.List.DefaultSkip,
+        [FromQuery] int take = Constants.List.DefaultPageSize,
+        CancellationToken cancellationToken = default)
+    {
+        var result = await _workoutsService.Get(userId, skip, take, cancellationToken);
+
+        return result.MatchFirst(
+            Results.Ok,
+            error => error.ToResponse()
+        );
+    }
+}
