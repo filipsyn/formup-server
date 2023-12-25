618c2ec7bdfc192f705bceaa4e5692b08703d107
Filip S <filip.syn@protonmail.com>
2023-12-25 14:37:02 +0100

    Add docs to `ApplyTranslation`

--- FormUp.Api/Features/v1/Translations/ITranslationService.cs
+++ FormUp.Api/Features/v1/Translations/ITranslationService.cs
@@ -2,7 +2,15 @@ namespace FormUp.Api.Features.v1.Translations;
 
 public interface ITranslationService
 {
-
-    public Task<bool> ApplyTranslation<TEntity>(TEntity entity, Func<TEntity, string> propertySelector,
+    /// <summary>
+    ///     Applies translation to entity.
+    /// </summary>
+    /// <param name="entity">An entity to which is translated.</param>
+    /// <param name="propertySelector">Properties which should be translated.</param>
+    /// <param name="language">A language to which content is translated.</param>
+    /// <typeparam name="TEntity">A type of entity which is being translated.</typeparam>
+    public Task<bool> ApplyTranslation<TEntity>(
+        TEntity entity,
+        Func<TEntity, string> propertySelector,
         string language);
 }
