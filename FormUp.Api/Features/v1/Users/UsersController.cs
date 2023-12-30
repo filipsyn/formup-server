e1a57646e8a6fd75dce968405cd7a403ffa4f237
Filip S <filip.syn@protonmail.com>
2023-12-30 21:50:05 +0100

    Add documentation to `UsersController` endpoints

--- FormUp.Api/Features/v1/Users/UsersController.cs
+++ FormUp.Api/Features/v1/Users/UsersController.cs
@@ -1,4 +1,5 @@
 using FormUp.Api.Common.Extensions;
+using FormUp.Api.Common.Models;
 using FormUp.Api.Features.v1.Shared;
 using FormUp.Contracts.v1.Users;
 
@@ -19,6 +20,8 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet(EndpointUrls.Users.GetByUid)]
+    [ProducesResponseType<ApiResponse<UserInfoResponse>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status404NotFound)]
     public async Task<IResult> GetByUid([FromRoute] string uid, CancellationToken cancellationToken = default)
     {
         var result = await _usersService.GetByUid(uid, cancellationToken);
@@ -30,6 +33,7 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet(EndpointUrls.Users.GetWeights)]
+    [ProducesResponseType<ApiResponse<IList<WeightLogResponse>>>(StatusCodes.Status200OK)]
     public async Task<IResult> GetWeights(
         [FromRoute] string uid,
         [FromQuery] DateTime? from = null,
@@ -42,6 +46,7 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet(EndpointUrls.Users.GetHeights)]
+    [ProducesResponseType<ApiResponse<IList<HeightLogResponse>>>(StatusCodes.Status200OK)]
     public async Task<IResult> GetHeights(
         string uid,
         DateTime? from = null,
@@ -54,6 +59,8 @@ public class UsersController : ControllerBase
     }
 
     [HttpPost(EndpointUrls.Users.Create)]
+    [ProducesResponseType<ApiResponse<CreateUserResponse>>(StatusCodes.Status201Created)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status400BadRequest)]
     public async Task<IResult> Create(CreateUserRequest request, CancellationToken cancellationToken = default)
     {
         var result = await _usersService.Create(request, cancellationToken);
@@ -65,6 +72,8 @@ public class UsersController : ControllerBase
     }
 
     [HttpPost(EndpointUrls.Users.LogWeight)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status201Created)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status400BadRequest)]
     public async Task<IResult> LogWeight([FromRoute] string uid, CreateWeightLogEntryRequest request,
         CancellationToken cancellationToken = default)
     {
@@ -77,6 +86,8 @@ public class UsersController : ControllerBase
     }
 
     [HttpPost(EndpointUrls.Users.LogHeight)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status201Created)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status400BadRequest)]
     public async Task<IResult> LogHeight(
         [FromRoute] string uid,
         CreateHeightLogEntryRequest request,
@@ -91,6 +102,8 @@ public class UsersController : ControllerBase
     }
 
     [HttpDelete(EndpointUrls.Users.Delete)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<ApiResponse>(StatusCodes.Status400BadRequest)]
     public async Task<IResult> Delete(string uid, CancellationToken cancellationToken = default)
     {
         var result = await _usersService.Delete(uid, cancellationToken);
