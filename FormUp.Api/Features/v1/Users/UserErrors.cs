b13f11c2347c8b725042ae5b9b2104e10bb8f469
Filip S <filip.syn@protonmail.com>
2023-12-30 18:39:32 +0100

    Refactor `UserErrors` to use error name in code

--- FormUp.Api/Features/v1/Users/UserErrors.cs
+++ FormUp.Api/Features/v1/Users/UserErrors.cs
@@ -6,11 +6,13 @@ public static class UserErrors
 {
     private const string FeaturePrefix = nameof(UserErrors);
 
-    public static Error NotFound => Error.NotFound($"{FeaturePrefix}:NotFound", "User was not found");
-    public static Error CreationFailure => Error.Failure($"{FeaturePrefix}:CreationFailure", "Unable to create user");
+    public static Error NotFound => Error.NotFound($"{FeaturePrefix}:{nameof(NotFound)}", "User was not found");
+
+    public static Error CreationFailure =>
+        Error.Failure($"{FeaturePrefix}:{nameof(CreationFailure)}", "Unable to create user");
 
     public static Error WeightLogFailure =>
-        Error.Failure($"{FeaturePrefix}:WeightLogFailure", "Could not add with weight log entry");
+        Error.Failure($"{FeaturePrefix}:{nameof(WeightLogFailure)}", "Could not add with weight log entry");
 
     public static Error HeightLogFailure =>
         Error.Failure($"{FeaturePrefix}:{nameof(HeightLogFailure)}", "Could not add height log entry");
