898ab61f54618b66afd6cfba891bd4e7ce9c8fee
Filip S <filip.syn@protonmail.com>
2024-01-02 15:55:20 +0100

    Add `Weight` and `Height` props to `CreateUserRequest`

--- FormUp.Contracts/v1/Users/CreateUserRequest.cs
+++ FormUp.Contracts/v1/Users/CreateUserRequest.cs
@@ -4,4 +4,6 @@ public record CreateUserRequest
 {
     public string Uid { get; set; }
     public string DisplayName { get; set; }
+    public float Weight { get; set; }
+    public float Height { get; set; }
 }
