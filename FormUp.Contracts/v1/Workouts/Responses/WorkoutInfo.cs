e9f7818d8f2a70dd7c4609f6b56c55700d870917
Filip S <filip.syn@protonmail.com>
2024-01-02 21:18:33 +0100

    Add `Name` and `Description` to contracts and mapping

--- FormUp.Contracts/v1/Workouts/Responses/WorkoutInfo.cs
+++ FormUp.Contracts/v1/Workouts/Responses/WorkoutInfo.cs
@@ -16,6 +16,16 @@ public record WorkoutInfo
     public string UserId { get; init; } = string.Empty;
 
     /// <summary>
+    ///     Name of the workout.
+    /// </summary>
+    public string? Name { get; set; } = String.Empty;
+
+    /// <summary>
+    ///     Description of the workout.
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
     ///     Date and time when workout took place.
     /// </summary>
     public DateTime At { get; init; } = DateTime.Now;
